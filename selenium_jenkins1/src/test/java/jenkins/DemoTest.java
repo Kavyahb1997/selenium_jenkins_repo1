b4a2495cedc8b96a93f47ecde1d3d981e017c0a2
User <User@DESKTOP-RG04IRQ>
2024-08-07 12:43:12 +0530

    first push

---
+++ selenium_jenkins1/src/test/java/jenkins/DemoTest.java
@@ -0,0 +1,23 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void sample()
+	{
+		Reporter.log("DemoTest class got executed in jenkins");
+		
+	}
+	
+	
+	
+
+
+
+
+
+}
+
+
